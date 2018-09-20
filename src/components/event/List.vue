f0cf3d67468e6dbfad9aeca3267aff241a5498c1
Rodrigo <cherry199662@gmail.com>
2018-09-20 10:36:44 -0500

    Adding List Event & View, Header

---
+++ src/components/event/List.vue
@@ -0,0 +1,41 @@
+<template>
+    <section id="blog" class="py-3">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <div class="card-columns">
+
+                        <div class="card">
+                            <img src="http://www.lorempixel.com/500/500/city/1" alt="" class="card-img-top img-fluid">
+                            <div class="card-body">
+                                <h4 class="card-title">Evento Nombre</h4>
+                                <small class="text-muted">Evento por realizar</small>
+                                <hr>
+                                <p class="card-text">Breve description del evento</p>
+                            </div>
+                        </div>
+
+                        <div class="card p-3">
+                            <blockquote class="car-body card-blockquote">
+                                <p>Invitacion : .</p>
+                                <footer>
+                                    <small class="text-muted">Personas encargadas o expositores<cite title="Source Title">Candidato</cite></small>
+                                </footer>
+                            </blockquote>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+  export default {
+    name: 'List',
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/includes/Header.vue
@@ -0,0 +1,28 @@
+<template>
+    <header id="page-header">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 m-auto text-center">
+                    <h1 v-once>{{title}}</h1>
+                    <p>{{subtitle}}</p>
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
+
+<script>
+  export default {
+    name: 'Header',
+    data() {
+      return {
+        title: '',
+        subtitle: ''
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
