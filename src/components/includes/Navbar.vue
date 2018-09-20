9fd5299c57e4f408ad345a5795265d0c6881a7d8
Rodrigo <cherry199662@gmail.com>
2018-09-20 05:01:17 -0500

    Integrating css and js for bootstrap

---
+++ src/components/includes/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+        <div class="container">
+            <a href="index.html" class="navbar-brand">Agenda UNJBG</a>
+            <button class="navbar-toggler" data-toggle="collapse" data-target="#navNavbar"><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+                        <a href="" class="nav-link">Inicio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="" class="nav-link">Acerca de nosotros</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="" class="nav-link">Servicios</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="" class="nav-link">Contacto</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+  export default {
+    name: 'Navbar',
+  }
+</script>
+
+<style scoped>
+
+</style>
